c338b17ba2a4cdb23c813a7de83940635222655b
Paulo Tozzi <paulo.tozzi@hubchain.io>
2019-10-28 12:37:24 -0300

    change

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'APP Clubemegadescontos' });
+  res.render('index', { title: 'Clubemegadescontos' });
 });
 
 module.exports = router;
