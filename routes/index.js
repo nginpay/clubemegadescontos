ea2340fb4fa7301acdf664e9c1e0a568c06024eb
Paulo Tozzi <paulo.tozzi@hubchain.io>
2019-10-31 00:23:59 -0300

    alteração endpoints

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,12 @@ router.get('/', function(req, res, next) {
 });
 
 /* GET home page. */
-router.get('/dashboard', function(req, res, next) {
-  res.render('home', { title: 'Dashboard' });
+router.get('/v1/dashboard', function(req, res, next) {
+  res.render('./dashboard/index', { title: 'Dashboard' });
 });
 
 /* GET Api page. */
-router.get('/api', function(req, res, next) {
+router.get('/v2/api', function(req, res, next) {
   res.json({ title: 'API v2' });
 });
 
