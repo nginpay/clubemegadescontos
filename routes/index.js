5f31eb4a8794d02db7e1602537102f015dc592b8
Paulo Tozzi <paulo.tozzi@hubchain.io>
2019-10-31 00:18:07 -0300

    application reset

--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Clubemegadescontos' });
+  res.render('index', { title: 'API Clubemegadescontos' });
 });
-
-/* GET dashboard home page. */
-router.get('/dashboard', function(req, res, next) {
-  res.render('dashboard', { title: 'Dashboard App' });
-});
-
-/* GET API home page. */
-router.get('/api', function(req, res, next) {
-  res.json({ title: 'Api Rest' });
-});
-
 
 module.exports = router;
