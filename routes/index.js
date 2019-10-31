c9348a41f0a71b7f62d35844736e4109f16e3cfa
Paulo Tozzi <paulo.tozzi@hubchain.io>
2019-10-31 00:40:29 -0300

    inclusão de paginas

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Clubemegadescontos' });
 });
 
-/* GET home page. */
-router.get('/dashboard', function(req, res, next) {
+/* GET login dashboard. */
+router.get('/login', function(req, res, next) {
   res.render('./dashboard/login', { title: 'Dashboard' });
 });
+
+/* GET home dashboard. */
+router.get('/dashboard', function(req, res, next) {
+  res.render('./dashboard/index', { title: 'Dashboard' });
+});
+
 
 /* GET Api page. */
 router.get('/api', function(req, res, next) {
