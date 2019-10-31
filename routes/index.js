de3d7d29db86848d95818bc598ba8f27c3d28705
Paulo Tozzi <paulo.tozzi@hubchain.io>
2019-10-31 00:20:52 -0300

    inclusão de endpoint

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'API Clubemegadescontos' });
+  res.render('index', { title: 'Clubemegadescontos' });
+});
+
+/* GET home page. */
+router.get('/dashboard', function(req, res, next) {
+  res.render('home', { title: 'Dashboard' });
+});
+
+/* GET Api page. */
+router.get('/api', function(req, res, next) {
+  res.json({ title: 'API v2' });
 });
 
 module.exports = router;
